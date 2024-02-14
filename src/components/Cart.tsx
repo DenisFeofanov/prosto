029709a2ddd3a416056847c78358beb63d0ef727
DenisFeofanov <omail4me@gmail.com>
2024-02-14 15:41:28 +0500

    [feat] show cart button only when there is something in cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,9 +3,12 @@
 import { useState } from "react";
 import CartButton from "./CartButton";
 import CartModal from "./CartModal";
+import { useAppSelector } from "@/lib/hooks";
+import { selectCart } from "@/lib/redux/cartSlice";
 
 function Cart() {
   const [isOpen, setIsOpen] = useState(false);
+  const cart = useAppSelector(selectCart);
 
   function toggleCart() {
     setIsOpen(!isOpen);
@@ -13,7 +16,7 @@ function Cart() {
 
   return (
     <>
-      <CartButton onClick={toggleCart} />
+      {cart.bouquets.length > 0 && <CartButton onClick={toggleCart} />}
       <CartModal isOpen={isOpen} closeModal={toggleCart} />
     </>
   );
